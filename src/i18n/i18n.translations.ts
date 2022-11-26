a03ae753e4f31272409e74b61c42db1e372ace49
miha-bhaskaran <athmiha@berkeley.edu>
2022-11-25 16:54:26 -0800

    Only i18n translate and no google translate

---
+++ src/i18n/i18n.translations.ts
@@ -0,0 +1,49 @@
+import { namespaces } from "./i18n.constants";
+
+export const en = {
+    [namespaces.common]: {
+      buttons: {
+        ok: "Ok",
+        cancel: "Cancel",
+      },
+    },
+    [namespaces.pages.hello]: {
+      welcome: "Welcome to Cultural Survival.",
+      categories: "Categories",
+      radio: "Radio",
+      grants: "Grants",
+      
+    },
+    [namespaces.pages.grants]: {
+      grants_feed: "Grants Available",
+      
+    },
+    [namespaces.pages.audio]: {
+      audio_feed: "AudioFEED_Feed",
+      
+    },
+  };
+
+export const es = {
+  [namespaces.common]: {
+    buttons: {
+      ok: "Aceptar",
+      cancel: "Cancelar",
+    },
+  },
+  [namespaces.pages.hello]: {
+    welcome: "Bienvenida a Cultural Survival.",
+    categories: "Categorías",
+    radio: "Radio",
+    grants: "Grants Feed",
+  
+  },
+  [namespaces.pages.grants]: {
+    grants_feed: "Subvenciones Disponibles",
+  },
+  [namespaces.pages.audio]: {
+    audio_feed: "AudioFIREBASE_Alimento",
+    
+  },
+};
+
--- src/screens/HomeScreen/Home.tsx
+++ src/screens/HomeScreen/Home.tsx
@@ -4,10 +4,66 @@ import RectButton from "../../components/RectButton";
 import globalStyles from "../../globalStyles";
 import styles from "./styles";
 
+import { useTranslation } from "react-i18next";
+import { namespaces } from "../../i18n/i18n.constants";
+import "../../i18n/i18n";
+
+import React, { useState, useEffect } from "react";
+
+
+import {
+  CREDENTIALS
+} from "@env";
+
+
+// Your credentials
+//const CREDENTIALS = JSON.parse(TEST);
+//const CREDENTIALS = process.env.CREDENTIALS;
+const CRED = JSON.parse(CREDENTIALS);
+
+
+{
+  "compilerOptions"; {
+    "allowJs"; true
+  }
+}
+var lang = 'en'
+//
+
 const HomeScreen = ({ navigation }: any) => {
+
+  const { i18n, t } = useTranslation(namespaces.pages.hello);
+  const [text, setText] = useState("");
+
+
+
+  function handleClick(lang: string) {
+    i18n.changeLanguage(lang);
+
+  }
+  
+  function toggle(lg: string) {
+    console.log(lang)
+    lang = (lg == 'en' ? 'es' : 'en')
+    console.log(lang)
+    return lang
+
+  }
+  
+
+
   return (
     <ViewContainer>
-      <Text style={globalStyles.h2}>Welcome {/*user?.emaiL*/}!</Text>
+
+      <Text style={globalStyles.h2}>{t("welcome")}</Text>
+      <RectButton 
+      text="toggle"
+      {...t("buttons.ok", { ns: namespaces.pages.hello })}
+      onPress={() => handleClick(toggle(lang))}
+      />
+
+
+
 
       <RectButton
         text="Sign Out"
