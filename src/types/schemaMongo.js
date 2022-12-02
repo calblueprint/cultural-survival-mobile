46ebeb2923db2bebcc81d8f4c3b98eccda4ee069
Alvaro Ortiz <varortz@berkeley.edu>
2022-12-01 18:07:46 -0800

    merged in final setup

---
+++ src/types/schemaMongo.js
@@ -0,0 +1,41 @@
+// import {ObjectId} from 'bson';
+
+// class Grant {
+//   constructor({
+//     amount,
+//     countries,
+//     deadline,
+//     description,
+//     duration,
+//     title,
+//     id = new ObjectId(),
+    
+//   }) {
+//     this._amount = amount;
+//     this.countries = countries;
+//     this.countries = countries;
+//     this.deadline = deadline;
+//     this.description = description;
+//     this.deadline = deadline;
+//     this.duration = duration;
+//     this.title = title;
+//   }
+
+//   static schema = {
+//     name: 'Grant',
+//     properties: {
+//         _id: 'objectId',
+//         amount: 'string',
+//         countries: 'string',
+//         deadline: 'date',
+//         description: 'string',
+//         duration: 'string',
+//         title: 'string'
+//     },
+//     primaryKey: '_id',
+//   };
+// }
+
+// export {Grant};
+
+
