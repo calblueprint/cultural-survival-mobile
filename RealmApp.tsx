8e4f1bab74e7760201acea32eb5f199e7289ba10
David Qing <41662609+davidqing6432@users.noreply.github.com>
2023-02-17 23:13:22 -0800

    Implement environment variables and assert typing (#10)
    * implemented env, added assertions
    
    * removing comments

--- RealmApp.tsx
+++ RealmApp.tsx
@@ -1,7 +1,10 @@
 import Realm from "realm";
-import { REACT_APP_ID } from "@env";
 
-const mongoDbId = REACT_APP_ID;
-const app = new Realm.App({ id: mongoDbId });
+let mongoId = process.env.REACT_APP_ID;
+if (!mongoId) {
+  console.error("No MongoDB App ID");
+  mongoId = mongoId = "";
+}
+const app = new Realm.App({ id: mongoId });
 
 export default app;
