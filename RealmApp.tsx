6f18173da8d9266a48f377d610fb6a8b8621bd29
David Qing <davidqing6432@gmail.com>
2023-02-17 22:09:23 -0800

    fixing app-id problem, cleaning gitignore

--- RealmApp.tsx
+++ RealmApp.tsx
@@ -1,6 +1,7 @@
 import Realm from "realm";
 import { REACT_APP_ID } from "@env";
 
-const app = new Realm.App({ id: REACT_APP_ID });
+const mongoDbId = REACT_APP_ID;
+const app = new Realm.App({ id: mongoDbId });
 
 export default app;
