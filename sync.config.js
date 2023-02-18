3619469c732b8cd78df220e8849d0dcb083ceefb
David Qing <davidqing6432@gmail.com>
2023-02-17 22:56:16 -0800

    implemented env, added assertions

--- sync.config.js
+++ sync.config.js
@@ -1,8 +1,13 @@
-import REACT_APP_ID from "@env"
+//import REACT_APP_ID from "@env"
 
+let mongoId = process.env.REACT_APP_ID;
+if (!mongoId) {
+  console.error("No MongoDB App ID");
+  mongoId = mongoId = "";
+}
 export const SYNC_CONFIG = {
 	// Set `enabled` to `true` to enable sync.
 	enabled: true,
 	// Add your Realm App ID here if sync is enabled.
-	appId: REACT_APP_ID,
+	appId: mongoId,
 };
