73c6fe64c5c0ca75e5eca2e020eb333d519b802e
David Qing <davidqing6432@gmail.com>
2023-02-17 21:58:11 -0800

    fixed spacing

--- App.tsx
+++ App.tsx
@@ -3,9 +3,9 @@ import RootNavigation from "./src/navigation";
 import { AuthProvider } from "./providers/AuthProvider";
 
 export default function App() {
-	return (
-		<AuthProvider>
-			<RootNavigation />
-		</AuthProvider>
-	);
+  return (
+    <AuthProvider>
+      <RootNavigation />
+    </AuthProvider>
+  );
 }
