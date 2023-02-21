4756b5d8a9c8ff82bbb397973994005c1ec17278
Pragya Kallanagoudar <pkallanagoudar@berkeley.edu>
2023-02-20 22:28:40 -0800

    runs prettier on home/welcome .tsx files

--- src/screens/HomeScreen/Home.tsx
+++ src/screens/HomeScreen/Home.tsx
@@ -1,16 +1,16 @@
-import { Text, View, Image, ScrollView, SafeAreaView } from "react-native";
-import ViewContainer from "../../components/ViewContainer";
-import RectButton from "../../components/RectButton";
-import globalStyles from "../../globalStyles";
-import styles from "./styles";
-import CategoryCard from "../../components/CategoryCard";
+import { Text, View, Image, ScrollView, SafeAreaView } from 'react-native';
+import ViewContainer from '../../components/ViewContainer';
+import RectButton from '../../components/RectButton';
+import globalStyles from '../../globalStyles';
+import styles from './styles';
+import CategoryCard from '../../components/CategoryCard';
 
-const HomeScreen = ({ navigation }: any) => {
-  const climateImage = require("../../../assets/climateChangeImage.png");
-  const languageImage = require("../../../assets/language.png");
-  const womenImage = require("../../../assets/women.png");
-  const educationImage = require("../../../assets/education.png");
-  const communicationImage = require("../../../assets/communication.png");
+function HomeScreen({ navigation }: any) {
+  const climateImage = require('../../../assets/climateChangeImage.png');
+  const languageImage = require('../../../assets/language.png');
+  const womenImage = require('../../../assets/women.png');
+  const educationImage = require('../../../assets/education.png');
+  const communicationImage = require('../../../assets/communication.png');
 
   return (
     <SafeAreaView style={styles.outerContainer}>
@@ -25,32 +25,32 @@ const HomeScreen = ({ navigation }: any) => {
         <View style={styles.container}>
           <CategoryCard
             name1="Climate Change & the Environment"
-            onPress1={() => navigation.navigate("Home")}
+            onPress1={() => navigation.navigate('Home')}
             img1={climateImage}
           />
           <CategoryCard
             name1="Communication"
-            onPress1={() => navigation.navigate("Home")}
+            onPress1={() => navigation.navigate('Home')}
             img1={communicationImage}
           />
           <CategoryCard
             name1="Language & Culture"
-            onPress1={() => navigation.navigate("Home")}
+            onPress1={() => navigation.navigate('Home')}
             img1={languageImage}
           />
           <CategoryCard
             name1="Education"
-            onPress1={() => navigation.navigate("Home")}
+            onPress1={() => navigation.navigate('Home')}
             img1={educationImage}
           />
           <CategoryCard
             name1="Land Rights"
-            onPress1={() => navigation.navigate("Home")}
+            onPress1={() => navigation.navigate('Home')}
             img1={educationImage}
           />
           <CategoryCard
             name1="Indeginous Women"
-            onPress1={() => navigation.navigate("Home")}
+            onPress1={() => navigation.navigate('Home')}
             img1={womenImage}
           />
         </View>
@@ -59,15 +59,15 @@ const HomeScreen = ({ navigation }: any) => {
       <RectButton
         text="Navigate"
         buttonStyle={{
-          margin: "5%",
-          backgroundColor: "#253C85",
-          alignSelf: "center",
+          margin: '5%',
+          backgroundColor: '#253C85',
+          alignSelf: 'center',
         }}
-        textStyle={{ color: "#FFF" }}
-        onPress={() => navigation.navigate("Welcome")}
+        textStyle={{ color: '#FFF' }}
+        onPress={() => navigation.navigate('Welcome')}
       />
     </SafeAreaView>
   );
-};
+}
 
 export default HomeScreen;
--- src/screens/HomeScreen/styles.tsx
+++ src/screens/HomeScreen/styles.tsx
@@ -1,30 +1,30 @@
-import { StyleSheet } from "react-native";
+import { StyleSheet } from 'react-native';
 
 export default StyleSheet.create({
   container: {
-    width: "100%",
+    width: '100%',
     // height: "100%",
     // backgroundColor: "red",
-    display: "flex",
-    flexWrap: "wrap",
-    flexDirection: "row",
-    justifyContent: "space-evenly",
-    paddingTop: "5%",
+    display: 'flex',
+    flexWrap: 'wrap',
+    flexDirection: 'row',
+    justifyContent: 'space-evenly',
+    paddingTop: '5%',
   },
 
   padding: {
-    padding: "2%",
-    alignSelf: "center",
+    padding: '2%',
+    alignSelf: 'center',
   },
 
   padding2: {
-    marginTop: "2%",
-    paddingLeft: "10%",
-    textAlign: "left",
+    marginTop: '2%',
+    paddingLeft: '10%',
+    textAlign: 'left',
   },
 
   outerContainer: {
     flex: 1,
-    backgroundColor: "#FAFAFA",
+    backgroundColor: '#FAFAFA',
   },
 });
