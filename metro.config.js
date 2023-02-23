90960dfb219b8728ce26ab6f6fb1c3354e244b47
David Qing <davidqing6432@gmail.com>
2023-02-22 20:59:12 -0800

    adding todo's to metro.config.js implementation

--- metro.config.js
+++ metro.config.js
@@ -1,4 +1,9 @@
 // Learn more https://docs.expo.io/guides/customizing-metro
+
 const { getDefaultConfig } = require('expo/metro-config');
+// TODO: Implement using an import statement instead of require. 
+// import { getDefaultConfig } from 'expo/metro-config';
 
 module.exports = getDefaultConfig(__dirname);
+// TODO: Implement using export statement instead of module.exports
+// export default getDefaultConfig(__dirname);
