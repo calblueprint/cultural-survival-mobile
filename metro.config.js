b662a78c2b6c3e62c02f0100010e4f8f49b6a9f3
David Qing <davidqing6432@gmail.com>
2023-02-22 21:00:21 -0800

    added suppression to eslint error for metro.config.js

--- metro.config.js
+++ metro.config.js
@@ -1,5 +1,6 @@
 // Learn more https://docs.expo.io/guides/customizing-metro
 
+// eslint-disable-next-line @typescript-eslint/no-var-requires
 const { getDefaultConfig } = require('expo/metro-config');
 // TODO: Implement using an import statement instead of require. 
 // import { getDefaultConfig } from 'expo/metro-config';
