b0427cd0559665c13d3c7b9353151e9c25188d6b
Miha <athmiha@berkeley.edu>
2023-02-24 12:05:53 -0800

    Resolved 7/9 prettier-esint errors/warnings

--- src/screens/SplashScreen/Splash.tsx
+++ src/screens/SplashScreen/Splash.tsx
@@ -1,16 +1,16 @@
-import { StyleSheet, Text, View } from "react-native";
+import React  from 'react';
+import { Text} from "react-native";
 import ViewContainer from "../../components/ViewContainer";
-import globalStyles from "../../globalStyles";
-import styles from "./styles";
 import Icon from "../../../assets/icons";
 
-const SplashScreen = ({ navigation }: any) => {
+
+function SplashScreen({ navigation }: any) {
 	return (
 		<ViewContainer>
 			<Icon type="grants_icon" />
 			<Text>This is a test screen!</Text>
 		</ViewContainer>
 	);
-};
+}
 
 export default SplashScreen;
