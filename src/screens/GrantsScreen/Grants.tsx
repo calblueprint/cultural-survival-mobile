af03d7af83d87fc857dee5a493cb7968200634ce
Alvaro Ortiz <varortz@berkeley.edu>
2023-02-26 17:49:34 -0800

    pre rebase

--- src/screens/GrantsScreen/Grants.tsx
+++ src/screens/GrantsScreen/Grants.tsx
@@ -1,42 +1,44 @@
-import { Text, View } from "react-native";
-import ViewContainer from "../../components/ViewContainer";
-import RectButton from "../../components/RectButton";
-import globalStyles from "../../globalStyles";
-import styles from "./styles";
+import { Text } from 'react-native';
+import { useTranslation } from 'react-i18next';
+import ViewContainer from '../../components/ViewContainer';
+import RectButton from '../../components/RectButton';
+import globalStyles from '../../globalStyles';
+import styles from './styles';
 
-import { useTranslation } from "react-i18next";
-import { namespaces } from "../../i18n/i18n.constants";
-import "../../i18n/i18n";
-import React, { useState, useEffect } from "react";
+import { namespaces } from '../../i18n/i18n.constants';
+import '../../i18n/i18n';
+
 {
-  "compilerOptions"; {
-    "allowJs"; true
+  ('compilerOptions');
+  {
+    ('allowJs');
+    true;
   }
 }
-var lang = 'en'
+let lang = 'en';
 const GrantsScreen = ({ navigation }: any) => {
   const { i18n, t } = useTranslation(namespaces.pages.grants);
   function handleClick(lang: string) {
     i18n.changeLanguage(lang);
   }
   function toggle(lg: string) {
-    lang = (lg == 'en' ? 'es' : 'en')
-    return lang
+    lang = lg === 'en' ? 'es' : 'en';
+    return lang;
   }
 
   return (
     <ViewContainer>
-      <Text style={globalStyles.h2}>{t("grants_feed")}</Text>
+      <Text style={globalStyles.h2}>{t('grants_feed')}</Text>
       <RectButton 
-      text="toggle"
-      {...t("buttons.ok", { ns: namespaces.pages.grants })}
-      onPress={() => handleClick(toggle(lang))}
+        text="toggle"
+        {...t('buttons.ok', { ns: namespaces.pages.grants })}
+        onPress={() => handleClick(toggle(lang))}
       />
       <RectButton
         text="Back"
-        buttonStyle={{ marginTop: "5%", backgroundColor: "#253C85" }}
-        textStyle={{ color: "#FFF" }}
-        onPress={() => navigation.navigate("Home")}
+        buttonStyle={{ marginTop: '5%', backgroundColor: '#253C85' }}
+        textStyle={{ color: '#FFF' }}
+        onPress={() => navigation.navigate('Home')}
       />
     </ViewContainer>
   );
