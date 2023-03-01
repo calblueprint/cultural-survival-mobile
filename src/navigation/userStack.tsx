f98f6979e46e16ecb7708010385cdb97c6db6c31
Annie Wang <wangannie@berkeley.edu>
2023-02-28 19:20:48 -0800

    [cleanup] revert unrelated changes

--- src/navigation/userStack.tsx
+++ src/navigation/userStack.tsx
@@ -2,9 +2,11 @@
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
 
+import HomeScreen from "../screens/HomeScreen/Home";
+import WelcomeScreen from "../screens/WelcomeScreen/Welcome";
 import AudioScreen from "../screens/AudioScreen/Audio";
 import GrantsScreen from "../screens/GrantsScreen/Grants";
-import HomeScreen from "../screens/HomeScreen/Home";
+import SplashScreen from "../screens/SplashScreen/Splash";
 import LoginScreen from "../screens/LoginScreen/Login";
 import SignIn1Screen from "../screens/SignIn1Screen/SignIn1";
 import SignIn2Screen from "../screens/SignIn2Screen/SignIn2";
@@ -12,11 +14,9 @@ import SignUp1Screen from "../screens/SignUp1Screen/SignUp1";
 import SignUp2Screen from "../screens/SignUp2Screen/SignUp2";
 import SignUp3Screen from "../screens/SignUp3Screen/SignUp3";
 import SignUp4Screen from "../screens/SignUp4Screen/SignUp4";
-import SplashScreen from "../screens/SplashScreen/Splash";
-import WelcomeScreen from "../screens/WelcomeScreen/Welcome";
 
-import { AuthProvider } from "../../providers/AuthProvider";
 import { RootStackParamList } from "../types/navigation";
+import { AuthProvider } from "../../providers/AuthProvider";
 
 const UserStack = () => {
   const Stack = createStackNavigator<RootStackParamList>();
