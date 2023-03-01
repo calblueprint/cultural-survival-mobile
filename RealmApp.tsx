49fcbe553dff601ba4714fbb4318b7920b19209b
Annie Wang <wangannie@berkeley.edu>
2023-02-28 18:20:34 -0800

    [cleanup] revert changes to realmapp, sync config

--- RealmApp.tsx
+++ RealmApp.tsx
@@ -1,9 +1,9 @@
-import Realm from 'realm';
+import Realm from "realm";
 
-let mongoId = 'process.env.REACT_APP_ID';
+let mongoId = process.env.REACT_APP_ID;
 if (!mongoId) {
-  console.error('No MongoDB App ID');
-  mongoId = mongoId = '';
+  console.error("No MongoDB App ID");
+  mongoId = mongoId = "";
 }
 const app = new Realm.App({ id: mongoId });
 
--- sync.config.js
+++ sync.config.js
@@ -1,11 +1,11 @@
-let mongoId = 'process.env.REACT_APP_ID';
+let mongoId = process.env.REACT_APP_ID;
 if (!mongoId) {
-  console.error('No MongoDB App ID');
-  mongoId = mongoId = '';
+  console.error("No MongoDB App ID");
+  mongoId = mongoId = "";
 }
 export const SYNC_CONFIG = {
-  // Set `enabled` to `true` to enable sync.
-  enabled: true,
-  // Add your Realm App ID here if sync is enabled.
-  appId: mongoId,
+	// Set `enabled` to `true` to enable sync.
+	enabled: true,
+	// Add your Realm App ID here if sync is enabled.
+	appId: mongoId,
 };
