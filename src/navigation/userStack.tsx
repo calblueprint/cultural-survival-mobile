50ca67db8928bb4d93b8c675b1a07a406045c775
Annie Wang <wangannie@berkeley.edu>
2023-03-06 02:01:33 -0800

    [feat] remove all references to realm incl. unnecessary auth screens

--- src/navigation/userStack.tsx
+++ src/navigation/userStack.tsx
@@ -1,43 +1,24 @@
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 
-import HomeScreen from '../screens/HomeScreen/Home';
-import WelcomeScreen from '../screens/WelcomeScreen/Welcome';
 import AudioScreen from '../screens/AudioScreen/Audio';
 import GrantsScreen from '../screens/GrantsScreen/Grants';
-import SplashScreen from '../screens/SplashScreen/Splash';
-import LoginScreen from '../screens/LoginScreen/Login';
-import SignIn1Screen from '../screens/SignIn1Screen/SignIn1';
-import SignIn2Screen from '../screens/SignIn2Screen/SignIn2';
-import SignUp1Screen from '../screens/SignUp1Screen/SignUp1';
-import SignUp2Screen from '../screens/SignUp2Screen/SignUp2';
-import SignUp3Screen from '../screens/SignUp3Screen/SignUp3';
-import SignUp4Screen from '../screens/SignUp4Screen/SignUp4';
+import HomeScreen from '../screens/HomeScreen/Home';
+import WelcomeScreen from '../screens/WelcomeScreen/Welcome';
 
 import { RootStackParamList } from '../types/navigation';
-import { AuthProvider } from '../../providers/AuthProvider';
 
 function UserStack() {
   const Stack = createStackNavigator<RootStackParamList>();
   return (
-    <AuthProvider>
-      <NavigationContainer>
-        <Stack.Navigator>
-          <Stack.Screen name="Home" component={HomeScreen} />
-          <Stack.Screen name="Welcome" component={WelcomeScreen} />
-          <Stack.Screen name="Login" component={LoginScreen} />
-          <Stack.Screen name="Splash" component={SplashScreen} />
-          <Stack.Screen name="Audio" component={AudioScreen} />
-          <Stack.Screen name="Grants" component={GrantsScreen} />
-          <Stack.Screen name="SignIn1" component={SignIn1Screen} />
-          <Stack.Screen name="SignIn2" component={SignIn2Screen} />
-          <Stack.Screen name="SignUp1" component={SignUp1Screen} />
-          <Stack.Screen name="SignUp2" component={SignUp2Screen} />
-          <Stack.Screen name="SignUp3" component={SignUp3Screen} />
-          <Stack.Screen name="SignUp4" component={SignUp4Screen} />
-        </Stack.Navigator>
-      </NavigationContainer>
-    </AuthProvider>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="Welcome" component={WelcomeScreen} />
+        <Stack.Screen name="Audio" component={AudioScreen} />
+        <Stack.Screen name="Grants" component={GrantsScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
--- src/types/navigation.tsx
+++ src/types/navigation.tsx
@@ -9,14 +9,6 @@ export type RootStackParamList = {
   Welcome: undefined;
   Audio: undefined;
   Grants: undefined;
-  SignIn1: undefined;
-  SignIn2: undefined;
-  Splash: undefined;
-  Login: undefined;
-  SignUp1: undefined;
-  SignUp2: undefined;
-  SignUp3: undefined;
-  SignUp4: undefined;
   Play: undefined;
 };
 
