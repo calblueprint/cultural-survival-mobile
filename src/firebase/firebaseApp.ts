196b3b99240eb6b7ec242936b0606ceb664958fc
David Qing <davidqing6432@gmail.com>
2023-03-10 01:00:45 -0800

    built app, added configuration, and queries demo as a screen.

--- src/firebase/firebaseApp.ts
+++ src/firebase/firebaseApp.ts
@@ -1,22 +1,22 @@
 import { initializeApp } from 'firebase/app';
-import {
-  FIREBASE_API_KEY,
-  FIREBASE_APP_ID,
-  FIREBASE_AUTH_DOMAIN,
-  FIREBASE_MEASUREMENT_ID,
-  FIREBASE_MESSAGING_SENDER_ID,
-  FIREBASE_PROJECT_ID,
-  FIREBASE_STORAGE_BUCKET,
-} from 'react-native-dotenv';
+// import {
+//   FIREBASE_API_KEY,
+//   FIREBASE_APP_ID,
+//   FIREBASE_AUTH_DOMAIN,
+//   FIREBASE_MEASUREMENT_ID,
+//   FIREBASE_MESSAGING_SENDER_ID,
+//   FIREBASE_PROJECT_ID,
+//   FIREBASE_STORAGE_BUCKET,
+// } from 'react-native-dotenv';
 
 const firebaseConfig = {
-  apiKey: FIREBASE_API_KEY,
-  authDomain: FIREBASE_AUTH_DOMAIN,
-  projectId: FIREBASE_PROJECT_ID,
-  storageBucket: FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: FIREBASE_MESSAGING_SENDER_ID,
-  appId: FIREBASE_APP_ID,
-  measurementId: FIREBASE_MEASUREMENT_ID,
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.FIREBASE_APP_ID,
+  measurementId: process.env.FIREBASE_MEASUREMENT_ID,
 };
 
 const firebaseApp = initializeApp(firebaseConfig);
