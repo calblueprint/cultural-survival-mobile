ebc51c979185a6b3292bc7c3e8bf67a73b070eee
Alvaro Ortiz <varortz@berkeley.edu>
2023-03-11 15:20:11 -0800

    attempt to integrate backend with frontend

--- src/screens/GrantsScreen/Grants.tsx
+++ src/screens/GrantsScreen/Grants.tsx
@@ -1,15 +1,18 @@
 import { useTranslation } from 'react-i18next';
-import { ScrollView, Text, View } from 'react-native';
+import { ScrollView, Text, View, TextInput, Button } from 'react-native';
 import GrantCard from '../../components/GrantCard';
 import RectButton from '../../components/RectButton';
 import ViewContainer from '../../components/ViewContainer';
 import globalStyles from '../../globalStyles';
 import styles from './styles';
+import React, {useEffect, useState} from 'React';
+import { Grant } from '../../types/schema'
 
 import '../../i18n/i18n';
 import { namespaces } from '../../i18n/i18n.constants';
 import Colors from '../../styles/Colors';
 import { RootStackScreenProps } from '../../types/navigation';
+import { getAllGrants , getGrantById} from '../../firebase/queries/grantsQueries';
 
 let lang = 'en';
 function GrantsScreen({ navigation }: RootStackScreenProps<'Grants'>) {
@@ -21,69 +24,68 @@ function GrantsScreen({ navigation }: RootStackScreenProps<'Grants'>) {
     lang = lg === 'en' ? 'es' : 'en';
     return lang;
   }
+  const [grants, setGrants] = useState<Grant[]>([]);
+  const [grantId, setGrantId] = useState<string>('');
+
+  /* fetch all grants on page load */
+  const handlePress = async () => {
+    const grant = await getGrantById(grantId);
+    // TODO grants-flow: remove this when handling getting grants by ID is done properly.
+    // eslint-disable-next-line no-console
+    console.log('Received grant: ', grant.title);
+  };
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const allGrants = await getAllGrants();
+        setGrants(allGrants);
+      } catch (error) {
+        // eslint-disable-next-line no-console
+        console.error('(useEffect)[QueriesDemo]', error);
+      }
+    };
+    fetchData();
+  }, []);
+
 
   return (
     <ScrollView style={styles.container}>
-      <ViewContainer>
+    <ViewContainer>
         <Text style={globalStyles.h2}>{t('grants_feed')}</Text>
-        <RectButton
+      <RectButton 
           text={t('buttons.ok', { ns: namespaces.pages.grants })}
-          buttonStyle={{}}
-          textStyle={{}}
-          onPress={() => handleClick(toggle(lang))}
-        />
-        <RectButton
-          text="Back"
-          buttonStyle={{
-            marginTop: '5%',
-            backgroundColor: Colors.bluePrimary,
-          }}
-          textStyle={{ color: Colors.textWhite }}
+          buttonStyle={{ backgroundColor: 'black' }}
+          textStyle={{ color: 'black' }}
+      onPress={() => handleClick(toggle(lang))}
+      />
+      <RectButton
+        text="Back"
+          buttonStyle={{ marginTop: '5%', backgroundColor: '#253C85' }}
+          textStyle={{ color: '#FFF' }}
           onPress={() => navigation.navigate('Home')}
-        />
-      </ViewContainer>
+      />
+    </ViewContainer>
 
       <View style={styles.grantsText}>
         <Text style={globalStyles.h1}>Grants Available</Text>
       </View>
 
       <ViewContainer>
-        <GrantCard
-          amount="$4059"
-          title="Indigenous Community Media Youth Fellowship"
-          countries="Bolivia, Ecuador, and Guatemala"
-          deadline="10/22/2023"
-          description="Brief: Providing opportunities for international Indigenous radio stations to strengthen their broadcast infrastructure and systems. The Indigenous Community /Media Fund also provides training opportunities in journalism, broadcasting."
-        />
-        <GrantCard
-          amount="$3000"
-          title="Keepers of the Earth Fund"
-          countries="Bolivia, Ecuador, and Peru"
-          deadline="1/18/2023"
-          description="Brief: KOEF provides small grants ranging between $500 and $5,000 USD to grassroots Indigenous-led organizations, collectives, and traditional governments in their self-determined development projects based on their Indigenous values."
-        />
-        <GrantCard
-          amount="$3050"
-          title="Indigenous Community Media Fund"
-          countries="Mexico, Nicaragua, Panama, and Guatemala"
-          deadline="08/13/2024"
-          description="Brief: Providing opportunities for international Indigenous radio stations to strengthen their broadcast infrastructure and systems. The Indigenous Community /Media Fund also provides training opportunities in journalism, broadcasting."
-        />
-        <GrantCard
-          amount="$2500"
-          title="Keepers of the Earth Fund"
-          countries="Puerto Rico, Haiti, and Guatemala"
-          deadline="07/26/2023"
-          description="Brief: KOEF provides small grants ranging between $500 and $5,000 USD to grassroots Indigenous-led organizations, collectives, and traditional governments in their self-determined development projects based on their Indigenous values."
-        />
-        <GrantCard
-          amount="$2000"
-          title="Indigenous Community Media Youth Fellowship"
-          countries="Nicaragua, Ecuador, and Guatemala"
-          deadline="05/09/2023"
-          description="Brief: Providing opportunities for international Indigenous radio stations to strengthen their broadcast infrastructure and systems. The Indigenous Community /Media Fund also provides training opportunities in journalism, broadcasting."
-        />
+        {grants.map(grant => (
+          <View key={grant.grant_id}>
+            <GrantCard 
+            amount={grant.amount.toString()}
+            title={grant.title}
+            countries={grant.countries.join(", ")}
+            deadline={grant.deadline.toString()} // need to look into this more & extracting date ONLY
+            description={grant.description}
+            />
+          </View>
+        ))}
       </ViewContainer>
+      <TextInput placeholder="ID to be queried" onChangeText={setGrantId} />
+      <Button title="Submit" onPress={() => handlePress()} />
     </ScrollView>
   );
 }
