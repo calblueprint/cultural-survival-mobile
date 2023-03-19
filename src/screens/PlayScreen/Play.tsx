6de0a08dec88d4805f4ceb9045cb000d9874bb83
Akshay Thakur <akshaythakur@berkeley.edu>
2023-03-19 16:45:15 -0700

    play/pause audio toggle functionality done

--- src/screens/PlayScreen/Play.tsx
+++ src/screens/PlayScreen/Play.tsx
@@ -1,11 +1,39 @@
 import { Image, Text, View } from 'react-native';
 import { SafeAreaView } from 'react-navigation';
+import { Audio } from 'expo-av';
+import React from 'react';
+import RectButton from '../../components/RectButton';
 import Icon from '../../../assets/icons';
 import Colors from '../../styles/Colors';
 import styles from './styles';
 import globalStyles from '../../globalStyles';
 
+async function toggleAudio(
+  sound: React.MutableRefObject<Audio.Sound>,
+  url: string,
+) {
+  try {
+    const result = await sound.current.getStatusAsync();
+    if (result.isLoaded) {
+      if (result.isPlaying === false) {
+        sound.current.playAsync();
+      } else {
+        sound.current.pauseAsync();
+      }
+    } else {
+      await sound.current.loadAsync({
+        uri: url,
+      });
+      sound.current.playAsync();
+    }
+  } catch (error) {
+    /* empty */
+  }
+}
+
 function PlayScreen() {
+  const sound = React.useRef(new Audio.Sound());
+
   return (
     <SafeAreaView style={globalStyles.container}>
       <View
@@ -39,6 +67,18 @@ function PlayScreen() {
         Green Colonization: An Interview with Maja Kristine Jama
       </Text>
       <Text style={styles.author_text}>Shaldon Ferris</Text>
+
+      <RectButton
+        text="Play"
+        buttonStyle={{ backgroundColor: 'black' }}
+        textStyle={{ color: 'white' }}
+        onPress={() =>
+          toggleAudio(
+            sound,
+            'https://storage.googleapis.com/download/storage/v1/b/cultural-survival-mobile.appspot.com/o/FernandDeVarennesUNSpecialRapporteurOnMinorityIssuesonIndigenousLanguages.mp3?generation=1678596979606352&alt=media',
+          )
+        }
+      />
 
       <View style={{ paddingLeft: '5%' }}>
         <Icon type="play_bar" />
