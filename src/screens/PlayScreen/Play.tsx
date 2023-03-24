57982d7f49d59341383be85008664c33216c7400
Akshay Thakur <akshaythakur@berkeley.edu>
2023-03-24 04:49:11 -0700

    saving audio modal done (no LocalStorage functionality)

--- src/screens/PlayScreen/Play.tsx
+++ src/screens/PlayScreen/Play.tsx
@@ -1,4 +1,4 @@
-import { Image, Text, TouchableWithoutFeedback, View } from 'react-native';
+import { Image, Modal, Pressable, Text, TouchableWithoutFeedback, View } from 'react-native';
 import { Audio } from "expo-av";
 import React, { useState } from 'react';
 import Svg, { Circle, Path, Rect } from 'react-native-svg';
@@ -6,7 +6,7 @@ import Icon from '../../../assets/icons';
 import Colors from '../../styles/Colors';
 import styles from './styles';
 
-function Play() {
+function play() {
   return (
     <Svg width="68" height="68" viewBox="0 0 68 68" fill="none">
       <Circle cx="34" cy="34" r="34" fill="#CC502F"/>
@@ -15,13 +15,32 @@ function Play() {
   )
 }
 
-function Pause() {
+function pause() {
   return (
     <Svg width="68" height="68" viewBox="0 0 68 68" fill="none">
       <Circle cx="34" cy="34" r="34" fill="#CC502F"/>
       <Path d="M39 21C39 20.4477 39.4477 20 40 20H45.5C46.0523 20 46.5 20.4477 46.5 21V47C46.5 47.5523 46.0523 48 45.5 48H40C39.4477 48 39 47.5523 39 47V21Z" fill="white"/>
       <Path d="M22 21C22 20.4477 22.4477 20 23 20H28.5C29.0523 20 29.5 20.4477 29.5 21V47C29.5 47.5523 29.0523 48 28.5 48H23C22.4477 48 22 47.5523 22 47V21Z" fill="white"/>
     </Svg>
+  )
+}
+
+function saved() {
+  return (
+    <Svg width="26" height="26" viewBox="0 0 26 26" fill="none">
+      <Path fillRule="evenodd" clipRule="evenodd" d="M7.58465 2.23193H18.418C20.2507 2.27337 21.7043 3.78996 21.668 5.62277V21.5261C21.6725 22.4099 21.16 23.2148 20.3572 23.5844C20.0836 23.7034 19.7888 23.766 19.4905 23.7686C18.9635 23.7654 18.457 23.5643 18.0713 23.2053L13.0013 18.5903L7.93132 23.2053C7.30662 23.7627 6.41669 23.9103 5.64548 23.5844C4.84268 23.2148 4.33011 22.4099 4.33465 21.5261V5.62277C4.29835 3.78996 5.75195 2.27337 7.58465 2.23193ZM14.4426 16.9867L19.5017 21.6017V5.62257C19.5328 4.98809 19.052 4.4447 18.4184 4.39841H7.58507C6.9515 4.4447 6.47063 4.98809 6.50173 5.62257V21.5259L11.5176 16.9867C12.3455 16.2293 13.6147 16.2293 14.4426 16.9867Z" fill="#CC502F"/>
+      <Path d="M6 5C6 4.44772 6.44772 4 7 4H19C19.5523 4 20 4.44772 20 5V17C20 17.5523 19.5523 18 19 18H7C6.44772 18 6 17.5523 6 17V5Z" fill="#CC502F"/>
+      <Path d="M12.698 11.6889C13.0983 11.3084 13.7313 11.3245 14.1118 11.7248L15.4896 13.1745C15.8701 13.5748 15.8541 14.2077 15.4538 14.5882L7.48067 22.1665C7.08035 22.547 6.44739 22.5309 6.06691 22.1306L4.68904 20.6809C4.30856 20.2806 4.32463 19.6477 4.72494 19.2672L12.698 11.6889Z" fill="#CC502F"/>
+      <Path d="M21.1211 18.7081C21.4963 19.1004 21.49 19.7203 21.107 20.1049L20.0346 21.1818C19.9674 21.2493 19.9102 21.3261 19.8647 21.4097L19.6308 21.8399C19.3142 22.4222 18.529 22.5409 18.0544 22.0783L10.0342 14.2619C9.63871 13.8765 9.63057 13.2434 10.016 12.8478L12.0688 10.7416C12.4639 10.3362 13.1163 10.3393 13.5076 10.7483L21.1211 18.7081Z" fill="#CC502F"/>
+    </Svg>
+  )
+}
+
+function notSaved() {
+  return (
+  <Svg width="26" height="26" viewBox="0 0 26 26" fill="none">
+    <Path fillRule="evenodd" clipRule="evenodd" d="M7.58465 2.23193H18.418C20.2507 2.27337 21.7043 3.78996 21.668 5.62277V21.5261C21.6725 22.4099 21.16 23.2148 20.3572 23.5844C20.0836 23.7034 19.7888 23.766 19.4905 23.7686C18.9635 23.7654 18.457 23.5643 18.0713 23.2053L13.0013 18.5903L7.93132 23.2053C7.30662 23.7627 6.41669 23.9103 5.64548 23.5844C4.84268 23.2148 4.33011 22.4099 4.33465 21.5261V5.62277C4.29835 3.78996 5.75195 2.27337 7.58465 2.23193ZM14.4426 16.9867L19.5017 21.6017V5.62257C19.5328 4.98809 19.052 4.4447 18.4184 4.39841H7.58507C6.9515 4.4447 6.47063 4.98809 6.50173 5.62257V21.5259L11.5176 16.9867C12.3455 16.2293 13.6147 16.2293 14.4426 16.9867Z" fill="#CC502F"/>
+  </Svg>
   )
 }
 
@@ -103,9 +122,40 @@ function PlayScreen() {
   }
   
   const sound = React.useRef(new Audio.Sound());
+  const [modalVisible, setModalVisible] = useState(false);
+  const [audioSaved, setAudioSaved] = useState(false);
+  
+  function toggleModal() {
+    setAudioSaved(!audioSaved);
+    setModalVisible(!modalVisible);
+    if (audioSaved) {
+      // TODO: remove from LocalStorage
+    } else {
+      // TODO: add to LocalStorage
+    }
+  }
   
   return (
     <View style={styles.container}>
+      <Modal
+        animationType="fade"
+        transparent
+        visible={modalVisible}
+        onRequestClose={() => {
+          setModalVisible(!modalVisible);
+        }}>
+        <View style={styles.modal}>
+          <View style={{alignItems: 'center', marginTop: 305}}>
+            <View style={styles.inset}>
+              <Text style={styles.inset_text1}>{audioSaved ? "Saved to Library!" : "Removed from Library!"}</Text>
+              <Pressable
+                onPress={() => setModalVisible(!modalVisible)}>
+                <Text style={styles.inset_text2}>OK</Text>
+              </Pressable>
+            </View>
+          </View>
+        </View>
+      </Modal>
       <View style={{flexDirection: 'column', alignItems: 'center', width: '100%', marginTop: 10}}>
         <View
           style={{
@@ -166,7 +216,7 @@ function PlayScreen() {
 
           <TouchableWithoutFeedback onPress={() => toggleAudio(sound, hardcodedResponse.url)}>
             <View style={{ width: 68, height: 68 }}>
-              {playState.isPlaying ? Pause() : Play()}
+              {playState.isPlaying ? pause() : play()}
             </View>
           </TouchableWithoutFeedback >
           
@@ -199,7 +249,11 @@ function PlayScreen() {
             marginBottom: 30,
           }}
         >
-          <Icon type="bookmark" />
+          <TouchableWithoutFeedback onPress={() => toggleModal()}>
+            <View style={{ width: 26, height: 26 }}>
+              {audioSaved ? saved() : notSaved()}
+            </View>
+          </TouchableWithoutFeedback >
           <Icon type="options" />
         </View>
       </View>
