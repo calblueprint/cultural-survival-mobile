aa82215eef2ec94cc00be3651bc667b9e1e72b4e
Alvaro Ortiz <varortz@berkeley.edu>
2023-03-26 02:55:00 -0700

    prettier/eslint changes to grant files

--- src/screens/GrantsScreen/Grants.tsx
+++ src/screens/GrantsScreen/Grants.tsx
@@ -1,32 +1,17 @@
-import { useTranslation } from 'react-i18next';
-import { ScrollView, Text, View, TextInput, Button } from 'react-native';
-import React, {useEffect, useState} from 'react';
+import { ScrollView, Text, View } from 'react-native';
+import { useEffect, useState } from 'react';
 import GrantCard from '../../components/GrantCard';
-import RectButton from '../../components/RectButton';
 import ViewContainer from '../../components/ViewContainer';
 import globalStyles from '../../globalStyles';
 import styles from './styles';
-import { Grant } from '../../types/schema'
+import { Grant } from '../../types/schema';
 
 import '../../i18n/i18n';
 import { RootStackScreenProps } from '../../types/navigation';
-import { getAllGrants , getGrantById} from '../../firebase/queries/grantsQueries';
-
+import { getAllGrants } from '../../firebase/queries/grantsQueries';
 
 function GrantsScreen({ navigation }: RootStackScreenProps<'Grants'>) {
-
   const [grants, setGrants] = useState<Grant[]>([]);
-  const [grantId, setGrantId] = useState<string>('');
-
-
-
-  /* fetch all grants on page load */
-  const handlePress = async () => {
-    const grant = await getGrantById(grantId);
-    // TODO grants-flow: remove this when handling getting grants by ID is done properly.
-    // eslint-disable-next-line no-console
-    console.log('Received grant: ', grant.title);
-  };
 
   useEffect(() => {
     const fetchData = async () => {
@@ -41,22 +26,22 @@ function GrantsScreen({ navigation }: RootStackScreenProps<'Grants'>) {
     fetchData();
   }, []);
 
-
   return (
     <ScrollView style={styles.container}>
       <ViewContainer>
-          <Text style={globalStyles.h2}>Grants Available</Text>
+        <Text style={globalStyles.h3}>Grants Available</Text>
       </ViewContainer>
 
       {grants.map(grant => (
-          <View key={grant.grant_id}>
-            <GrantCard 
+        <View key={grant.grant_id}>
+          <GrantCard
             grantObj={grant}
-            onPress={() => navigation.navigate('GrantInfo', {grantObj : grant})}
-            />
-          </View>
-        ))}
-
+            onPress={() =>
+              navigation.navigate('GrantInfo', { grantObj: grant })
+            }
+          />
+        </View>
+      ))}
     </ScrollView>
   );
 }
