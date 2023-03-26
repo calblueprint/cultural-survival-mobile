f5860cca9b642be898121fcecc7c6ad60f29f1a4
Alvaro Ortiz <varortz@berkeley.edu>
2023-03-26 02:55:00 -0700

    prettier/eslint changes to grant files

--- src/screens/GrantInfoScreen/GrantInfo.tsx
+++ src/screens/GrantInfoScreen/GrantInfo.tsx
@@ -1,22 +1,17 @@
-
-import { Image, Text, View } from 'react-native';
+import { Text, View } from 'react-native';
 import globalStyles from '../../globalStyles';
 
-
-
-export default function GrantInfo({ route, navigation }) {
-    const { grantObj } = route.params;
-    return (
-        <View>
-            <Text style={globalStyles.h2}>{grantObj.title}</Text>
-            <Text>{grantObj.title}</Text>
-            <Text>{`$${  grantObj.amount.toString()}`}</Text>
-            <Text>{grantObj.countries.join(", ")}</Text>
-            <Text>{grantObj.description}</Text>
-            <Text>{grantObj.duration}</Text>
-            <Text>{(new Date(grantObj.deadline)).toLocaleDateString()}</Text>
-        </View>
-
-
-    )    
+export default function GrantInfo({ route }) {
+  const { grantObj } = route.params;
+  return (
+    <View>
+      <Text style={globalStyles.h2}>{grantObj.title}</Text>
+      <Text>{grantObj.title}</Text>
+      <Text>{`$${grantObj.amount.toString()}`}</Text>
+      <Text>{grantObj.countries.join(', ')}</Text>
+      <Text>{grantObj.description}</Text>
+      <Text>{grantObj.duration}</Text>
+      <Text>{new Date(grantObj.deadline).toLocaleDateString()}</Text>
+    </View>
+  );
 }
