531e850d39dd42e8fc9aa0f89043cf73951a105f
Alvaro Ortiz <varortz@berkeley.edu>
2023-04-02 21:26:36 -0700

    specified grant fields for testing purposes

--- src/screens/GrantInfoScreen/GrantInfo.tsx
+++ src/screens/GrantInfoScreen/GrantInfo.tsx
@@ -9,12 +9,11 @@ export default function GrantInfo({
   return (
     <View>
       <Text style={globalStyles.h2}>{grantObj.title}</Text>
-      <Text>{grantObj.title}</Text>
-      <Text>{`$${grantObj.amount.toString()}`}</Text>
-      <Text>{grantObj.countries.join(', ')}</Text>
-      <Text>{grantObj.description}</Text>
-      <Text>{grantObj.duration}</Text>
-      <Text>{new Date(grantObj.deadline).toLocaleDateString()}</Text>
+      <Text>Amount: {`$${grantObj.amount.toString()}`}</Text>
+      <Text>Eligible Countries: {grantObj.countries.join(', ')}</Text>
+      <Text>Brief: {grantObj.description}</Text>
+      <Text>Duration: {grantObj.duration}</Text>
+      <Text>Deadline: {new Date(grantObj.deadline).toLocaleDateString()}</Text>
     </View>
   );
 }
