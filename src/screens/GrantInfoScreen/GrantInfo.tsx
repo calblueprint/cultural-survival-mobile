f67e0d123583f416a13a98f3f7f51778aeacfa9b
David Qing <davidqing6432@gmail.com>
2023-04-03 18:39:57 -0700

    added basic styles for grantsinfo

--- src/screens/GrantInfoScreen/GrantInfo.tsx
+++ src/screens/GrantInfoScreen/GrantInfo.tsx
@@ -1,11 +1,12 @@
 import { Text, View } from 'react-native';
 import globalStyles from '../../globalStyles';
 import { GrantsStackScreensProps } from '../../types/navigation';
+import styles from './styles';
 
 function GrantInfoScreen({ route }: GrantsStackScreensProps<'GrantInfo'>) {
   const { grantObj } = route.params;
   return (
-    <View>
+    <View style={styles.container}>
       <Text style={globalStyles.h2}>{grantObj.title}</Text>
       <Text>Amount: {`$${grantObj.amount.toString()}`}</Text>
       <Text>Eligible Countries: {grantObj.countries.join(', ')}</Text>
