c5f6436a03b24168042fb1374a964ed68b7ec3a3
Alvaro Ortiz <varortz@berkeley.edu>
2023-04-09 18:55:33 -0700

    added icons + color changes

--- src/components/GrantCard.tsx
+++ src/components/GrantCard.tsx
@@ -6,18 +6,13 @@ import { Grant } from '../types/schema';
 import globalStyles from '../globalStyles';
 
 const styles = StyleSheet.create({
-  // centeredView: {
-  //   flex: 1,
-  //   justifyContent: 'center',
-  //   alignItems: 'center',
-  //   marginTop: 16,
-  // },
+
   cardContainer: {
     flexDirection: 'column',
     alignItems: 'flex-start',
-    backgroundColor: Colors.redPrimary,
+    backgroundColor: Colors.surfaceGrey,
     width: 350,
-    height: 140,
+    height: 270,
     borderRadius: 12,
     justifyContent: 'space-between',
     marginVertical: 4,
@@ -27,81 +22,27 @@ const styles = StyleSheet.create({
     shadowOffset: { width: 2, height: 4 },
     display: 'flex',
   },
-  whiteText: {
-    color: Colors.textWhite,
+  blackText: {
+    color: Colors.textPrimary,
     margin: 16,
     marginVertical: 8,
     textAlign: 'left',
     justifyContent: 'space-between',
     fontSize: 15,
   },
-  // modalBodyText: {
-  //   color: Colors.textPrimary,
-  //   margin: 8,
-  //   marginLeft: 16,
-  //   textAlign: 'left',
-  //   justifyContent: 'space-between',
-  //   fontSize: 15,
-  // },
   titleText: {
-    color: Colors.textWhite,
+    color: Colors.textPrimary,
     margin: 16,
     marginBottom: 8,
     textAlign: 'left',
     fontSize: 20,
   },
-  // closeButtonText: {
-  //   color: Colors.textWhite,
-  //   margin: 8,
-  //   marginLeft: 16,
-  //   textAlign: 'center',
-  //   justifyContent: 'space-between',
-  //   fontSize: 15,
-  // },
-  // modalView: {
-  //   margin: 20,
-  //   backgroundColor: Colors.surfaceWhite,
-  //   borderRadius: 13,
-  //   width: 340,
-  //   padding: 30,
-  //   shadowColor: Colors.shadowDark,
-  //   shadowOffset: {
-  //     width: 0,
-  //     height: 2,
-  //   },
-  //   shadowOpacity: 0.25,
-  //   shadowRadius: 4,
-  //   elevation: 5,
-  // },
-  // button: {
-  //   borderRadius: 5,
-  //   padding: 10,
-  //   elevation: 2,
-  // },
-  // buttonClose: {
-  //   backgroundColor: Colors.redPrimary,
-  //   width: '96%',
-  //   justifyContent: 'center',
-  // },
-  // modalTitle: {
-  //   fontSize: 22,
-  //   fontWeight: '400',
-  //   textAlign: 'left',
-  //   color: Colors.textPrimary,
-  //   marginLeft: '3%',
-  //   marginBottom: '8%',
-  // },
-  // innerModal: {
-  //   display: 'flex',
-  //   justifyContent: 'space-between',
-  //   marginBottom: 19,
-  //   width: '96%',
-  // },
-  // images: {
-  //   margin: 6,
-  //   display: 'flex',
-  //   justifyContent: 'center',
-  // },
+  infoContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    // alignSelf: 'center',
+
+  }
 });
 
 type GrantCardProps = {
@@ -114,7 +55,7 @@ function GrantCard({ grantObj, onPress }: GrantCardProps) {
     <ViewContainer>
       <TouchableHighlight
         // TODO: Fix coloring, add to default colors
-        underlayColor="#942000"
+        underlayColor={Colors.surfaceGreyClicked}
         onPress={onPress}
         style={styles.cardContainer} // also need to update this to reflect new design changes (create new branch for new frontend!!)
       >
@@ -122,12 +63,16 @@ function GrantCard({ grantObj, onPress }: GrantCardProps) {
           <Text style={[globalStyles.h2, styles.titleText]}>
             {grantObj.title}
           </Text>
-          <Text style={styles.whiteText}>
-            {`$${grantObj.amount.toString()}`}
-          </Text>
-          <Text style={styles.whiteText}>
-            Due Date: {new Date(grantObj.deadline).toLocaleDateString()}{' '}
-          </Text>
+          <View style={styles.infoContainer}>
+            <Icon type='coins'/>
+            <Text style={styles.blackText}>
+              Amount: {`$${grantObj.amount.toString()}`}
+            </Text>
+            <Icon type='calendar'/>
+            <Text style={styles.blackText}>
+              Due Date: {new Date(grantObj.deadline).toLocaleDateString()}{' '}
+            </Text>
+          </View>
         </View>
       </TouchableHighlight>
       <Icon type="expand" />
